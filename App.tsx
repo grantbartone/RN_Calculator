9a457eb91bbe5bcb273fedd588b265f5aeae298f
31108963+grantbartone@users.noreply.github.com <gbartone@gmail.com>
2022-12-15 13:39:38 -0800

    rough layout and styling

--- App.tsx
+++ App.tsx
@@ -1,9 +1,77 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [display, setDisplay] = useState("0");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.row}>
+        <Pressable style={[styles.button, styles.funcButton, styles.oneColumn]}>
+          <Text>AC</Text>
+        </Pressable>
+        <Text style={styles.display}>{display}</Text>
+      </View>
+
+      <View style={styles.row}>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>7</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>8</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>9</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.funcButton, styles.oneColumn]}>
+          <Text>/</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.row}>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>4</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>5</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>6</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.funcButton, styles.oneColumn]}>
+          <Text>*</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.row}>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>1</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>2</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>4</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.funcButton, styles.oneColumn]}>
+          <Text>-</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.row}>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>0</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>.</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.oneColumn]}>
+          <Text>=</Text>
+        </Pressable>
+        <Pressable style={[styles.button, styles.funcButton, styles.oneColumn]}>
+          <Text>+</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -11,8 +79,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingBottom: 100,
+    backgroundColor: "black",
+    color: "white",
+  },
+  row: { flexDirection: "row", height: 100 },
+  oneColumn: { flex: 1 },
+  twoColumn: { flex: 2 },
+  threeColumn: { flex: 3 },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    backgroundColor: "gray",
+  },
+  funcButton: { backgroundColor: "orange" },
+  display: {
+    flex: 3,
+    borderWidth: 1,
+    textAlign: "right",
+    color: "white",
+    fontSize: 50,
+    lineHeight: "100%",
+    marginRight: 10,
   },
 });
